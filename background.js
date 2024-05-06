7f852294f3aa6a0a11bc6d3222971afdf3e87355
yeada <yeadalm04@gmail.com>
2024-05-07 04:10:14 +0900

    ADD 시뮬레이션 페이지 만들고, 수강 성적 불러오기

--- background.js
+++ background.js
@@ -15,6 +15,42 @@ function fetchData() {
   .catch(error => {
     console.error('취득 학점 정보를 가져오는데 실패하였습니다:', error);
   });
+
+  fetch('https://klas.kw.ac.kr/std/cps/inqire/AtnlcScreSungjukInfo.do', {
+  method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({})
+  })
+  .then(response => response.json())
+  .then(data => {
+    // 데이터를 chrome.storage에 저장
+    chrome.storage.local.set({ SungjukInfo: data }, (    ) => {
+    });
+    console.log("성적Info : ", data);
+  })
+  .catch(error => {
+    console.error('성적 정보를 가져오는데 실패하였습니다:', error);
+  });
+
+  fetch('https://klas.kw.ac.kr/std/cps/inqire/AtnlcScreHakjukInfo.do', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({})
+  })
+  .then(response => response.json())
+  .then(data => {
+    // 데이터를 chrome.storage에 저장
+    chrome.storage.local.set({ HakjukInfo: data }, (    ) => {
+    });
+    console.log("HakjukInfo : ", data);
+  })
+  .catch(error => {
+    console.error('학적 정보를 가져오는데 실패하였습니다:', error);
+  });
 }
 
   
@@ -37,6 +73,9 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
         break;
       case "경영학부":
         openNewTab("html/business.html", message.hakbun);
+        break;
+      case "성적계산기":
+        openNewTab("html/calculator.html");
         break;
       default:
         console.log("해당 학과 정보를 처리할 수 없습니다.");
