8a63c16b1ffa897bb3f5540e5b05df3ddff07e13
yeada <yeadalm04@gmail.com>
2024-05-07 01:21:03 +0900

    ADD 전공, 전체 이수 학점 background에서 불러오기

--- background.js
+++ background.js
@@ -1,9 +1,21 @@
-// 데이터 저장 예시
-// var valueToSave = 'value'; // 저장할 값 정의
-chrome.storage.local.set({key: 'value'}, function() {
-  console.log('Value is set to ' + valueToSave); // 수정된 부분
-});
-
+function fetchData() {
+  fetch('https://klas.kw.ac.kr/std/cps/inqire/AtnlcScreSungjukTot.do', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({})
+  })
+  .then(response => response.json())
+  .then(data => {
+    // 데이터를 chrome.storage에 저장
+    chrome.storage.local.set({currentScores: data}, () => {
+    });
+  })
+  .catch(error => {
+    console.error('취득 학점 정보를 가져오는데 실패하였습니다:', error);
+  });
+}
 
   
 
@@ -13,12 +25,11 @@ function openNewTab(url, hakbun) {
   chrome.tabs.create({ url: newURL });
 }
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  console.log("백그라운드");
+  fetchData();
 
   if (message.action === "openNewTab") {
     switch(message.hakgwa) {
       case "정보융합학부":
-        console.log("정융 백그라운드 도착");
         openNewTab("html/informationConvergence.html", message.hakbun);
         break;
       case "전자공학과":
@@ -34,4 +45,3 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     console.log("알 수 없는 액션입니다.");
   }
 });
-
